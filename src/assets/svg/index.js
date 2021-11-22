2a6db15517ada39f834a2ca94fc3deb34efbeafd
Steph <otenku123456@gmail.com>
2021-11-22 07:18:01 +0100

    loading

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,1 +1,3 @@
 export { ReactComponent as Search } from "./search.svg";
+export { ReactComponent as LoadingWhite } from "./loading-white.svg";
+export { ReactComponent as LoadingBlack } from "./loading-black.svg";
--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 
 //components
 import { Header, LazyImage, Search } from "../components";
+import { LoadingWhite, LoadingBlack } from "../assets/svg";
 
 // app context
 import AppContext from "../context";
@@ -26,40 +27,44 @@ const Result = () => {
           <div className="mt-60 ring-1 max-w-max rounded-lg">
             <Search />
           </div>
-          <div
-            className="w-full mt-10 ring-gray-600 ring-2 rounded-sm py-8 px-8"
-            style={{ height: "fit-content" }}
-          >
-            {!error && animal?.Animals ? (
-              <>
-                <h1 className="text-6xl font-bold mb-5 w-full break-words">
-                  {animal?.Animals}
-                </h1>
-                <span className="italic font-thin text-2xl">
-                  {animal["Transcription English"]}
-                </span>
-
-                <span className="mt-8 block font-bold text-4xl mb-3">
-                  {animal["Translation (Yoruba)"]}
-                </span>
-                <span className="font-thin italic text-2xl">
-                  {animal["Transcription Yoruba"]}
-                </span>
-                <div className="mt-10">
-                  <span className="font-bold mb-5 text-2xl text-gray-900">
-                    Description
+          {loading ? (
+            <LoadingBlack />
+          ) : (
+            <div
+              className="w-full mt-10 ring-gray-600 ring-2 rounded-sm py-8 px-8"
+              style={{ height: "fit-content" }}
+            >
+              {!error && animal?.Animals ? (
+                <>
+                  <h1 className="text-6xl font-bold mb-5 w-full break-words">
+                    {animal?.Animals}
+                  </h1>
+                  <span className="italic font-thin text-2xl">
+                    {animal["Transcription English"]}
                   </span>
-                  <p className="font-thin text-xl italics">
-                    {animal?.Description}
-                  </p>
-                </div>
-              </>
-            ) : (
-              <span className="font-bold mb-5 text-2xl text-gray-900">
-                {error}
-              </span>
-            )}
-          </div>
+
+                  <span className="mt-8 block font-bold text-4xl mb-3">
+                    {animal["Translation (Yoruba)"]}
+                  </span>
+                  <span className="font-thin italic text-2xl">
+                    {animal["Transcription Yoruba"]}
+                  </span>
+                  <div className="mt-10">
+                    <span className="font-bold mb-5 text-2xl text-gray-900">
+                      Description
+                    </span>
+                    <p className="font-thin text-xl italics">
+                      {animal?.Description}
+                    </p>
+                  </div>
+                </>
+              ) : (
+                <span className="font-bold mb-5 text-2xl text-gray-900">
+                  {error}
+                </span>
+              )}
+            </div>
+          )}
         </div>
         {!loading ? (
           <LazyImage
@@ -70,7 +75,9 @@ const Result = () => {
             onload={onload}
           ></LazyImage>
         ) : (
-          <div className="w-6/12 h-full bg-gray-700"></div>
+          <div className="w-6/12 h-full bg-gray-700 flex items-center justify-center">
+            <LoadingWhite />
+          </div>
         )}
       </div>
     </div>
